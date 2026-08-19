c3b6c26b43c72c23eca93b344541faf7046fca6f
Rezatahony-boop <Rezatahony@yahoo.com>
2026-08-20 01:02:22 +0330

    Create MainActivity.kt

---
+++ app/src/main/java/ir/inspectionmanager/MainActivity.kt
@@ -0,0 +1,25 @@
+package ir.inspectionmanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.hilt.navigation.compose.hiltViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import ir.inspectionmanager.presentation.viewmodel.InspectionViewModel
+import ir.inspectionmanager.ui.navigation.AppNavigation
+import ir.inspectionmanager.ui.theme.InspectionManagerTheme
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            InspectionManagerTheme {
+                val viewModel: InspectionViewModel = hiltViewModel()
+                AppNavigation(viewModel)
+            }
+        }
+    }
+}
